f17529641f21e208785a3e26bb2632ed91c8dab7
Joaquin Ogallar <joaquinnogallar@gmail.com>
2025-09-16 21:57:39 -0300

    first commit

---
+++ src/main/java/com/joaquinogallar/mcserverdemo/McServerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.joaquinogallar.mcserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McServerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McServerDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joaquinogallar/mcserverdemo/McServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaquinogallar.mcserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McServerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
